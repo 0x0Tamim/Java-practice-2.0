e54e1321e431b6fdf2d948e1fd40c23c49061eb4
0x0Tamim <mmtamimuzzaman@gmail.com>
2025-04-29 23:23:25 +0600

    Added corrected code

--- CountTotalVowelsConsonantsDigitsSpaces/src/Main.java
+++ CountTotalVowelsConsonantsDigitsSpaces/src/Main.java
@@ -1,17 +1,36 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.print("Enter a string: ");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        input = scanner.nextLine();
+        int vowels=0,consonants=0,digits=0,spaces = 0;
+        input = input.toLowerCase();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        for(char ch: input.toCharArray()){
+            if(Character.isDigit(ch)){
+                digits++;
+            }
+            else if(Character.isWhitespace(ch)){
+                spaces++;
+
+            }
+            else if(ch>='a' && ch<= 'z'){
+                if("aeiou".indexOf(ch) != -1){
+                    vowels++;
+                }
+                else consonants++;
+            }
         }
+
+        System.out.println("Total Digit: "+digits);
+        System.out.println("Total Vowel: "+vowels);
+        System.out.println("Total Consonant: "+consonants);
+        System.out.println("Total Spaces: "+spaces);
+
     }
 }
