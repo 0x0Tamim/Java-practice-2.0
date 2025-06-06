8a51c2693b09a781a82ab04e8eedf493aa9a3c0c
0x0Tamim <mmtamimuzzaman@gmail.com>
2025-06-06 16:44:03 +0600

    Updated Grocery List Main.java and added Flight Seat Planner project

--- Java Basic Practices/Grocery List/src/Main.java
+++ Java Basic Practices/Grocery List/src/Main.java
@@ -1,17 +1,29 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+      Scanner sc = new Scanner(System.in);
+        ArrayList<String> item = new ArrayList<>();
+        System.out.println("Enter items to add, if done type (done) ");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        while(true) {
+            System.out.print("Add: ");
+            String a = sc.nextLine().trim().toLowerCase(Locale.ROOT);
+            if (a.isEmpty()) {
+                System.out.println("Item cannot be empty.");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            }
+            else if(a.equals("done"))
+                break;
+            else if (item.contains(a)) {
+                System.out.println("Item already exist.");
+            } else
+                item.add(a);
+
         }
+        System.out.println("Grocery List: "+item);
     }
 }
