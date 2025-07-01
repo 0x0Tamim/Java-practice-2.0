32d6a358124af6b1363e8715c9f3e11be7571875
0x0Tamim <mmtamimuzzaman@gmail.com>
2025-07-02 00:03:21 +0600

    Updated median logic to take user input

--- LeetCode Problems/Median of Two Sorted Array/src/Main.java
+++ LeetCode Problems/Median of Two Sorted Array/src/Main.java
@@ -1,13 +1,10 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
+        int i = 0, j = 0, k = 0;
         int[] mix = new int[nums1.length + nums2.length];
 
-        // Merge two sorted arrays
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i] <= nums2[j]) {
                 mix[k++] = nums1[i++];
@@ -16,7 +13,6 @@ class Solution {
             }
         }
 
-        // Add remaining elements
         while (i < nums1.length) {
             mix[k++] = nums1[i++];
         }
@@ -25,7 +21,6 @@ class Solution {
             mix[k++] = nums2[j++];
         }
 
-        // Find median
         int n = mix.length;
         if (n % 2 == 0) {
             return (mix[n / 2 - 1] + mix[n / 2]) / 2.0;
@@ -37,12 +32,30 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Solution sol = new Solution();
 
-        int[] nums1 = {1, 3};
-        int[] nums2 = {2};
+        // Input for first array
+        System.out.print("Enter number of elements in first array: ");
+        int n1 = sc.nextInt();
+        int[] nums1 = new int[n1];
+        System.out.println("Enter " + n1 + " elements (sorted):");
+        for (int i = 0; i < n1; i++) {
+            nums1[i] = sc.nextInt();
+        }
+
+        // Input for second array
+        System.out.print("Enter number of elements in second array: ");
+        int n2 = sc.nextInt();
+        int[] nums2 = new int[n2];
+        System.out.println("Enter " + n2 + " elements (sorted):");
+        for (int i = 0; i < n2; i++) {
+            nums2[i] = sc.nextInt();
+        }
 
         double median = sol.findMedianSortedArrays(nums1, nums2);
         System.out.println("Median is: " + median);
+
+        sc.close();
     }
 }
